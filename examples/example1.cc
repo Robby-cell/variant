5672c426fc6508c55fc4d13e873cfc31f777f065
Robert <robert11122002@gmail.com>
2025-07-02 14:18:07 +0100

    Added example

--- examples/example1.cc
+++ examples/example1.cc
@@ -1,1 +1,20 @@
-int main() {}
+#include <ios>
+#include <iostream>
+#include <string>
+
+#include "variant/variant.hpp"
+
+int main() {
+    using variant::Variant;
+
+    Variant<int, double, char> v;
+    v.Emplace<int>(42);
+
+    using variant::HoldsAlternative, variant::Visit;
+
+    auto str = Visit([](const auto& v) { return std::to_string(v); }, v);
+    std::cout << "Variant to string = " << str << '\n';
+
+    auto holds_int = HoldsAlternative<int>(v);
+    std::cout << "Holds int: " << std::boolalpha << holds_int << '\n';
+}
