e451188d17974aafa77ab9d303bfa530405b8bfd
Robert <robert11122002@gmail.com>
2025-07-02 16:33:31 +0100

    Update example for C++14

--- examples/example1.cc
+++ examples/example1.cc
@@ -4,15 +4,23 @@
 
 #include "variant/variant.hpp"
 
+struct Visitor {
+    template <class Ty>
+    std::string operator()(const Ty& v) const {
+        return std::to_string(v);
+    }
+};
+
 int main() {
     using variant::Variant;
 
     Variant<int, double, char> v;
     v.Emplace<int>(42);
 
-    using variant::HoldsAlternative, variant::Visit;
+    using variant::HoldsAlternative;
+    using variant::Visit;
 
-    auto str = Visit([](const auto& v) { return std::to_string(v); }, v);
+    auto str = Visit(Visitor{}, v);
     std::cout << "Variant to string = " << str << '\n';
 
     auto holds_int = HoldsAlternative<int>(v);
